c059af23004f426e5c7f5d50850cfb389c51bd43
Scott Feldman-Peabody <peabody@scotts-air.lan>
2019-11-10 20:15:26 -0700

    app2 with intial search function working

--- app/app2.py
+++ app/app2.py
@@ -30,11 +30,11 @@ category_list = list(get_table(sql).iloc[:,0])
 # home page
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    properties = ['CLTTB', 'DALLF', 'LRDES', 'DTTLI', 'CHILM', 'SEALW','MEMPE', 'MIASP', 'YULDN', 'MCOOR']
+    
     categories = category_list
     months = month_list
     nums = [5, 1, 2, 3, 4, 6, 7, 8, 9, 10]
-    return render_template('index.html', properties = properties, categories = categories, months = months, nums = nums)
+    return render_template('index.html', categories = categories, months = months, nums = nums)
 
 @app.route('/recommend', methods=['GET', 'POST'])
 def recommend():
@@ -53,10 +53,38 @@ def recommend():
 
     return render_template('recommend.html', property=property, recommendation=recommendation)
 
-@app.route('/property_lookup')
+@app.route('/property_lookup', methods=['GET', 'POST'])
+
 def property_lookup():
-    prop_lookup = get_table('SELECT * FROM property_code_lookup')
-    return render_template('property_lookup.html', data = prop_lookup.to_html())
+    sql = 'SELECT DISTINCT flag_name FROM product_category_recommender ORDER BY flag_name'
+    flags = []
+    flags.append(None)
+    flags = flags + list(get_table(sql).iloc[:,0])
+    sql = 'SELECT DISTINCT city FROM product_category_recommender ORDER BY city'
+    cities = [None] + list(get_table(sql).iloc[:,0])
+    sql = 'SELECT DISTINCT state FROM product_category_recommender ORDER BY state'
+    states = [None] + list(get_table(sql).iloc[:,0])
+
+    city = None
+    state = None
+
+    if request.method == 'POST':
+        city = request.form['city']
+        prop_lookup = get_table("SELECT * FROM property_code_lookup WHERE city = '{}' ".format(city))
+    #     if request.form['city']:
+    #         city = request.form['city']
+    #     state = request.form['state']
+    #     flag_name = request.form['flag_name']
+    # if city != None:
+    #     prop_lookup = get_table("SELECT * FROM property_code_lookup WHERE city = '{}' ".format(city))
+    # if state != None:
+    #     prop_lookup = get_table("SELECT * FROM property_code_lookup WHERE state = '{}' ".format(state))
+    # if flag_name != None:
+    #     prop_lookup = get_table("SELECT * FROM property_code_lookup WHERE flag_name = '{}' ".format(flag_name))
+    else:
+        prop_lookup = get_table('SELECT * FROM property_code_lookup')
+    return render_template('property_lookup.html', data = prop_lookup.to_html(), \
+        flags = flags, cities = cities, states = states)
 
 @app.route('/about')
 def about():
