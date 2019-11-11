41dfeeeb8886db27fa66fea27154667106125a64
Scott Feldman-Peabody <peabody@Scotts-MacBook-Air.local>
2019-11-11 12:08:28 -0700

    price optimizer code working

--- src/product_sales.py
+++ src/product_sales.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from scipy.interpolate import CubicSpline, UnivariateSpline, interp1d
+from scipy import interpolate, stats
 
-class prod_sales(object):
+class prod_subset(object):
     '''
     Looks at perfomance of indiviudal products
     
@@ -18,6 +20,11 @@ class prod_sales(object):
         return_df() : returns a DatFrame of the subsetted data
         plot_sales() :  returns number sold and revenue as a function of price
         plot_dists() : plots the distribution of price, number sold, and revenue
+        
+    Variables:
+        _best_price : estimated best price to set on an item to maximize profits
+        _best_revenue : the estimated median revenue when using the best price
+        _best_sales : the estimated median number of sales when using the best price
     '''
     
     def __init__(self, df, description, month = None, cluster = None):
@@ -27,14 +34,43 @@ class prod_sales(object):
         self.cluster = cluster
         self.data = df[df.description == description]
         if cluster:
-            self.data = df[df.cluster == cluster]
+            self.data = self.data[self.data.cluster == self.cluster]
+        if month:
+            self.data = self.data[self.data.transaction_month == self.month]
         self.data = self.data.groupby('unit_price').mean()[['number_sold']].reset_index()
         self.data['revenue'] = self.data.unit_price * self.data.number_sold
+        self._best_price, self._best_rev, self._best_sales = self.optimize_price()
         
     def return_df(self):
         return self.data
     
-    def plot_sales(self):
+    def optimize_price(self):
+        if self.data.shape[0] < 2:
+            return None, None, None
+        else:
+            x = self.data['unit_price']
+            y0 = self.data['number_sold']
+            y1 = self.data['revenue']
+            
+            mn = round(min(x))
+            mx = np.ceil(max(x))
+            rng = mx - mn
+            price_bins = np.linspace(mn,mx,(rng * 4) +1)
+            bin_x = np.digitize(x, price_bins, right=True)
+            mean,std=norm.fit(bin_x)
+            y_fit = norm.pdf(bin_x, mean, std)
+            best_price = price_bins[np.unique(bin_x)][bin_x[np.argmax(y_fit)]]
+            if np.max(stats.binned_statistic(x, y1, statistic='median')[0]):
+                max_rev = np.max(stats.binned_statistic(x, y1, statistic='median')[0])
+            else:
+                max_rev = np.median(y1[bin_x == bin_x[np.argmax(y_fit)]])
+            if np.max(stats.binned_statistic(x, y0, statistic='median')[0]):
+                max_sales = np.max(stats.binned_statistic(x, y0, statistic='median')[0])
+            else:
+                max_sales = np.median(y0[bin_x == bin_x[np.argmax(y_fit)]])
+            return best_price, max_rev, max_sales
+    
+    def scatplot(self):
     
         if self.data.shape[0] < 2:
             return 'Not enough sales for selected product to plot'
@@ -59,19 +95,74 @@ class prod_sales(object):
         ax[1].set_ylabel('Average Revenue')
         ax[1].plot(x, spl1(x), c = 'b', label='Spline Fit')
         ax[1].legend()
-        if self.cluster:
+        if self.cluster != None:
             clust_descrip = ' | Cluster: {}'.format(self.cluster)
         else:
             clust_descrip = ''
         plt.suptitle(self.description + clust_descrip)
+        fig.savefig('../img/sales_scatter.png')
     
-    def plot_dists(self):
-        fig, ax = plt.subplots(1,3, figsize = (12,4))
-        
-        cols = list(self.data.columns)
+    def boxplots(self):
+        if self.data.shape[0] < 1:
+            return "Not enough sales for selected product to plot"
+        else:
+            x = self.data['unit_price']
+            y0 = self.data['number_sold']
+            y1 = self.data['revenue']
             
-        for i, col in enumerate(cols):
-            ax[i] = sns.distplot(self.data[col], bins = 15, ax = ax[i])
-            ax[i].set
-            ax[i].set_title = col
-        plt.tight_layout()
+            mn = round(min(x))
+            mx = np.ceil(max(x))
+            rng = mx - mn
+            price_bins = np.linspace(mn,mx,(rng * 4) +1)
+            bin_x = np.digitize(x, price_bins, right=True)
+            mean,std=norm.fit(bin_x)
+            y_fit = norm.pdf(bin_x, mean, std)
+
+            fig, ax = plt.subplots(1,2, figsize = (12,4))
+            
+            sns.boxplot(x = bin_x, y = y0, color = 'lightblue', ax = ax[0])
+            sns.stripplot(x = bin_x, y = y0, color = 'red', size = 3, ax = ax[0])
+            ax[0].plot(bin_x, (y_fit/np.max(y_fit))*self._best_sales, c = 'green') # scale to box plot
+            
+            ax[0].set_title('Quantity vs. Price Bins')
+            ax[0].set_xticklabels(price_bins[np.unique(bin_x)], rotation = 60)
+            ax[0].set_xlabel('Price')
+            ax[0].set_ylabel('Number Sold')
+            
+            sns.boxplot(x = bin_x, y = y1, color = 'lightblue', ax = ax[1])
+            sns.stripplot(x = bin_x, y = y1, color = 'red', size = 3, ax = ax[1])
+            ax[1].plot(bin_x, (y_fit/np.max(y_fit))*self._best_rev, c = 'green') # scale to box plot
+    
+            ax[1].set_title('Revenue vs. Price Bins')
+            ax[1].set_xticklabels(price_bins[np.unique(bin_x)], rotation = 60)
+            ax[1].set_xlabel('Price')
+            ax[1].set_ylabel('Revenue')
+            
+            if self.cluster != None:
+                clust_descrip = ' | Cluster: {}'.format(self.cluster)
+            else:
+                clust_descrip = ''
+            plt.suptitle(self.description + clust_descrip)
+            fig.savefig('../img/sales_boxplot.png')
+    
+    def dists(self):
+        
+        if self.data.shape[0] < 2:
+            return 'Not enough sales for selected product to plot'
+        else:
+            fig, ax = plt.subplots(1,3, figsize = (12,4))
+
+            cols = list(self.data.columns)
+
+            for i, col in enumerate(cols):
+                #ax[i].hist(self.data[col])
+                ax[i] = sns.distplot(self.data[col], bins = 15, ax = ax[i])
+                ax[i].set_ylabel = 'col'
+            ax[0].set_ylabel = 'col'
+            if self.cluster != None:
+                clust_descrip = ' | Cluster: {}'.format(self.cluster)
+            else:
+                clust_descrip = ''
+            plt.suptitle('Distributions for ' + self.description + clust_descrip)
+            fig.savefig('../img/sales_distributions.png')
+          
